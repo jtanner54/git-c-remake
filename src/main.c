42eed5f6f8cfd145ecfefa6fd1a2bd69010a65ef
Jack Tanner <jtanner0786@gmail.com>
2025-10-17 13:27:41 -0400

    git init

--- src/main.c
+++ src/main.c
@@ -17,27 +17,31 @@ int main(int argc, char *argv[]) {
     const char *command = argv[1];
     
     if (strcmp(command, "init") == 0) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        fprintf(stderr, "Logs from your program will appear here!\n");
-
-        // Uncomment this block to pass the first stage
-        // 
-        // if (mkdir(".git", 0755) == -1 || 
-        //     mkdir(".git/objects", 0755) == -1 || 
-        //     mkdir(".git/refs", 0755) == -1) {
-        //     fprintf(stderr, "Failed to create directories: %s\n", strerror(errno));
-        //     return 1;
-        // }
-        // 
-        // FILE *headFile = fopen(".git/HEAD", "w");
-        // if (headFile == NULL) {
-        //     fprintf(stderr, "Failed to create .git/HEAD file: %s\n", strerror(errno));
-        //     return 1;
-        // }
-        // fprintf(headFile, "ref: refs/heads/main\n");
-        // fclose(headFile);
-        // 
-        // printf("Initialized git directory\n");
+        /*link for mode_t: https://man7.org/linux/man-pages/man2/chmod.2.html
+        it's basically a bitmask that's ORing together diff RWX perms
+        https://jameshfisher.com/2017/02/24/what-is-mode_t/
+        octal representation
+        000 111 000 000 RWX mask for owner
+        000 000 101 000 R and X for group
+        000 000 000 101 R and X for other
+        000 000 000 000 no id or swapped text*/
+        
+        if (mkdir(".git", 0755) == -1 || 
+            mkdir(".git/objects", 0755) == -1 || 
+            mkdir(".git/refs", 0755) == -1) {
+            fprintf(stderr, "Failed to create directories: %s\n", strerror(errno));
+            return 1;
+        }
+        
+        FILE *headFile = fopen(".git/HEAD", "w");
+        if (headFile == NULL) {
+            fprintf(stderr, "Failed to create .git/HEAD file: %s\n", strerror(errno));
+            return 1;
+        }
+        fprintf(headFile, "ref: refs/heads/main\n");
+        fclose(headFile);
+        
+        printf("Initialized git directory\n");
     } else {
         fprintf(stderr, "Unknown command %s\n", command);
         return 1;
